426340649039209f7f9e87300236a63648a563c4
Asg Software <softwareasg@gmail.com>
2026-07-05 07:40:39 +0530

    Sleek email prompt and 30-day session

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
   secret: 'taskwhite_super_secret',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 week
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
 }));
 
 // Setup global variables for views
